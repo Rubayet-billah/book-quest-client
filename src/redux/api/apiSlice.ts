07984af1d7d83dbf6323607f2c1ea77665a308d4
Rubayet-billah <rubayetbillah909@gmail.com>
2023-07-18 01:43:59 +0600

    added live url

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,7 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+//  production => "https://book-quest-server.vercel.app/api/v1"
+// development => "http://localhost:5000/api/v1";
+
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-quest-server.vercel.app/api/v1",
+  }),
   endpoints: () => ({}),
 });
