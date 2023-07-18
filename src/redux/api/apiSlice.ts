7cc7f46867f6252fa5047979dae758aeea451949
Rubayet-billah <rubayetbillah909@gmail.com>
2023-07-18 22:36:59 +0600

    url set

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://book-quest-server.vercel.app/api/v1",
   }),
   tagTypes: ["Book", "Wishlist"],
   endpoints: () => ({}),
